f7d8fe4a2cccd7063d94eac4b44a41bb09a0d578
Jakub Kuźnik (jakukuz389) <jakukuz389@student.polsl.pl>
2018-03-14 23:50:59 +0100

    Przenioslem zliczanie job time do sinka.

--- lab1/zad1/sink.cc
+++ lab1/zad1/sink.cc
@@ -1,16 +1,41 @@
 #include <omnetpp.h>
+#include "FileUtil.h"
+
 using namespace omnetpp;
 class Sink : public cSimpleModule
 {
   protected:
+  	virtual void initialize();
     virtual void handleMessage(cMessage *msgin);
+    virtual void finish();
 };
 
 
 Define_Module(Sink);
 
 
+	long jobInSystemCount;
+	double jobsTime;
+
+void Sink::initialize()
+{
+	jobsTime = 0;
+	jobInSystemCount = 0;
+}
+
+void Sink::finish()
+{
+	double mean = jobsTime/jobInSystemCount;
+
+    std::ostream &stream = FileUtil::getInstance().getFile(par("file_name"));
+    stream << "Sredni czas zadania w systemie = " << mean << "\n";
+    stream << "Zakonczenie symulacji" << "\n\n";
+}
+
+
 void Sink::handleMessage(cMessage *msgin)
 {
+	jobInSystemCount++;
+	jobsTime += (simTime().dbl() - msgin->getTimestamp().dbl());
 	delete msgin;
 }
